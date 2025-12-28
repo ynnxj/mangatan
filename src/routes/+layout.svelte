e9939216221693a61d718bb04e638d11f6d2908b
Jennifer Nilsson <jennifer.maritza.nilsson@gmail.com>
2025-12-28 17:47:34 +0100

    🎨 chore(setup): create basic folder structure

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,12 +1,6 @@
 <script lang="ts">
-  import favicon from '$lib/assets/favicon.svg';
-
   let { children } = $props();
 </script>
-
-<svelte:head>
-  <link rel="icon" href={favicon} />
-</svelte:head>
 
 <nav>
   <a href="/">home</a>
