870304f091af75b06d673ae51b8183cdfa6d6c1f
Jennifer Nilsson <jennifer.maritza.nilsson@gmail.com>
2025-12-30 17:32:35 +0100

    🎨 refactor(layout) update layout
    Add header and footer components.
    Move nav component inside main.

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,12 +1,17 @@
 <script lang="ts">
   import '../app.scss';
   import Nav from '$lib/Nav/Nav.svelte';
+  import Header from '$lib/Header/Header.svelte';
+  import Footer from '$lib/Footer/Footer.svelte';
 
   let { children } = $props();
 </script>
 
+<Header />
+
 <main>
   {@render children()}
+  <Nav />
 </main>
 
-<Nav />
+<Footer />
