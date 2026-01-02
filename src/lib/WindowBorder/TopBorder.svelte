fdf11df62315e05ac03c7a0ed91a3c76a7baf669
Jennifer Nilsson <jennifer.maritza.nilsson@gmail.com>
2026-01-02 12:52:36 +0100

    ✨ feat(window): create reusable window border
    Create Window98-style windows.
    WIP.

---
+++ src/lib/WindowBorder/TopBorder.svelte
@@ -0,0 +1,40 @@
+<script lang="ts">
+  import MinimizeIcon from '$lib/Icons/MinimizeIcon.svg';
+  import WindowIcon from '$lib/Icons/WindowIcon.svg';
+  import CloseIcon from '$lib/Icons/CloseIcon.svg';
+
+  export let title: string = 'Window Title';
+</script>
+
+<div class="window-top-border">
+  <span>{title}</span>
+  <div class="window-left">
+    <img src={MinimizeIcon} alt="Minimize" />
+    <img src={WindowIcon} alt="Maximize" />
+    <img src={CloseIcon} alt="Close" />
+  </div>
+</div>
+
+<style lang="scss">
+  .window-top-border {
+    border: 2px solid var(--win98-white);
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    color: var(--win98-white);
+    font-weight: bold;
+    background-color: var(--win98-blue);
+
+    span {
+      margin-left: 1rem;
+    }
+
+    .window-left {
+      display: flex;
+      gap: 0.5rem;
+      padding: 0.25rem;
+      background-color: white;
+      border-radius: 2px;
+    }
+  }
+</style>
---
+++ src/lib/WindowBorder/Window.svelte
@@ -0,0 +1,30 @@
+<script lang="ts">
+  import TopBorder from './TopBorder.svelte';
+</script>
+
+<div class="window">
+  <TopBorder title="About Us" />
+  <div class="window-content">
+    <slot />
+  </div>
+</div>
+
+<style lang="scss">
+  .window {
+    position: relative;
+    width: 600px;
+    height: 400px;
+    border-top: 2px solid var(--win98-white);
+    border-left: 2px solid var(--win98-white);
+    border-right: 2px solid var(--win98-darker-gray);
+    border-bottom: 2px solid var(--win98-darker-gray);
+    background: var(--win98-gray);
+    border-top: 2px solid var(--win98-white);
+
+    .window-content {
+      flex: 1;
+      overflow: auto;
+      padding: 10px;
+    }
+  }
+</style>
