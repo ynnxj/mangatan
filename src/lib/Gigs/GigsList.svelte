ceb603dd563f965bc84533bec58b72bd464c8c70
Jennifer Nilsson <jennifer.maritza.nilsson@gmail.com>
2026-01-02 16:22:15 +0100

    ✨ feat(gigs): create default gigs page
    wip.

---
+++ src/lib/Gigs/GigsList.svelte
@@ -0,0 +1,13 @@
+<script>
+  import Window from '$lib/WindowBorder/Window.svelte';
+</script>
+
+<Window windowTitle="Gigs">
+  <h2>Gigs</h2>
+  <!-- TODO: Add gigs data and render-->
+  <ul>
+    <li>Gig 1 - Date - Location</li>
+    <li>Gig 2 - Date - Location</li>
+    <li>Gig 3 - Date - Location</li>
+  </ul>
+</Window>
--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -1,5 +1,19 @@
 <script lang="ts">
   import AboutBand from '$lib/About/Band/AboutBand.svelte';
+  import GigsList from '$lib/Gigs/GigsList.svelte';
 </script>
 
-<AboutBand />
+<section>
+  <AboutBand />
+  <GigsList />
+</section>
+
+<style lang="scss">
+  section {
+    display: flex;
+    flex-direction: column;
+    gap: 20px;
+    align-items: center;
+    margin: 20px 0;
+  }
+</style>
