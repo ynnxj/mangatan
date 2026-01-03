dfa6259a1a237bbd4095c03e4db452db6a662da6
Jennifer Nilsson <jennifer.maritza.nilsson@gmail.com>
2026-01-03 22:00:23 +0100

    ✨ feat(contact): update form and add styling
    wip.

--- src/lib/Form/Form.svelte
+++ src/lib/Form/Form.svelte
@@ -1,3 +1,7 @@
+<script>
+  import Window from '$lib/WindowBorder/Window.svelte';
+</script>
+
 <!--TODO: Add accessibility attributes to all form elements 
 and ensure proper labeling for screen readers -->
 <form method="post" action="/contact">
@@ -18,40 +22,58 @@ and ensure proper labeling for screen readers -->
 
 <style lang="scss">
   form {
+    margin-top: 10px;
     display: flex;
     flex-direction: column;
-    max-width: 400px;
 
     label {
       margin-top: 1em;
+      font-family: 'Orbitron', sans-serif;
+      font-weight: 500;
+    }
+
+    textarea {
+      height: 100px;
     }
 
     input,
     textarea {
-      margin-top: 0.5em;
-      padding: 0.5em;
+      margin-top: 5px;
+      padding: 10px;
       font-size: 1em;
+      width: 100%;
+      font-family: inherit;
     }
 
     .contact-buttons {
       display: flex;
-      justify-content: space-between;
-      gap: 1em;
-    }
+      justify-content: end;
+      gap: 15px;
+      margin: 15px 0;
 
-    // TODO: Refactor button styles later
-    button {
-      margin-top: 1.5em;
-      padding: 0.75em;
-      font-size: 1em;
-      background-color: #007bff;
-      color: white;
-      border: none;
-      cursor: pointer;
-      width: 100%;
+      button {
+        padding: 10px 30px;
+        font-size: 1.2rem;
+        font-family: 'Orbitron', sans-serif;
+        align-self: center;
+        text-shadow: 1px 1px 2px black;
+        border: 1px solid #f3f3f3;
+        box-shadow: 0 0 0 1px #707070;
+        border-radius: 15px;
+        background: linear-gradient(to bottom, #f2f2f2 0%, #d4d6e6 50%, #8996bb 51%, #cfcfcf 100%);
+        color: white;
 
-      &:hover {
-        background-color: #0056b3;
+        &:hover {
+          border: 1px solid #ecf7fd;
+          box-shadow: 0 0 0 1px #3c7fb1;
+          background: linear-gradient(
+            to bottom,
+            #eaf6fd 0%,
+            #d9f0fc 50%,
+            #bee6fd 51%,
+            #a7d9f5 100%
+          );
+        }
       }
     }
   }
--- src/routes/contact/+page.svelte
+++ src/routes/contact/+page.svelte
@@ -1,25 +1,163 @@
 <script lang="ts">
   import Form from '$lib/Form/Form.svelte';
+  import Window from '$lib/WindowBorder/Window.svelte';
+  import instagram from '$lib/Icons/instagram.gif';
+  import youtube from '$lib/Icons/youtube.png';
+  import spotify from '$lib/Icons/spotify.gif';
 </script>
 
+<!--TODO: Fix class for Window. This is a v messy solution -->
 <section>
-  <h2>Contact Us</h2>
   <div class="contact-container">
-    <Form />
-    <div>
-      <img src="/assets/images/group-portrait-cold.jpg" class="contact-img" alt="Band portrait" />
+    <div class="left-column">
+      <Window windowTitle={'Contact Us'}>
+        <h3 class="form-title">Send a Message</h3>
+        <Form />
+      </Window>
+
+      <div class="social-media-window">
+        <Window windowTitle={'Follow Us!'}>
+          <h3 class="social-media-title">⊹₊ ˚‧︵‿₊୨୧₊‿︵‧ ˚ ₊⊹ Social Media .ೃ࿔*:･</h3>
+          <div class="social-media-container">
+            <div class="stamps">
+              <a href="https://www.instagram.com/mangatanband/" title="Instagram" target="_blank"
+                ><img src={instagram} alt="instagram icon" /></a
+              >
+              <a
+                href="https://open.spotify.com/artist/04sRSWXovcYM8oSfApH4a6"
+                title="Spotify"
+                target="_blank"><img src={spotify} alt="spotify icon" /></a
+              >
+              <a
+                href="https://www.youtube.com/channel/UCs3M4AnSaBZ8AiqLlUD4q5w"
+                title="YouTube"
+                target="_blank"><img src={youtube} alt="youtube icon" /></a
+              >
+            </div>
+          </div>
+        </Window>
+      </div>
     </div>
+
+    <Window windowTitle={'A Message'}>
+      <div class="right">
+        <div class="text">
+          <pre class="cat">
+  ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⡀⠀⠀⠀⠀
+⠀⠀⠀⠀⢀⡴⣆⠀⠀⠀⠀⠀⣠⡀⠀⠀⠀⠀⠀⠀⣼⣿⡗⠀⠀⠀⠀
+⠀⠀⠀⣠⠟⠀⠘⠷⠶⠶⠶⠾⠉⢳⡄⠀⠀⠀⠀⠀⣧⣿⠀⠀⠀⠀⠀
+⠀⠀⣰⠃⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢻⣤⣤⣤⣤⣤⣿⢿⣄⠀⠀⠀⠀
+⠀⠀⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣧⠀⠀⠀⠀⠀⠀⠙⣷⡴⠶⣦
+⠀⠀⢱⡀⠀⠉⠉⠀⠀⠀⠀⠛⠃⠀⢠⡟⠀⠀⠀⢀⣀⣠⣤⠿⠞⠛⠋
+⣠⠾⠋⠙⣶⣤⣤⣤⣤⣤⣀⣠⣤⣾⣿⠴⠶⠚⠋⠉⠁⠀⠀⠀⠀⠀⠀
+⠛⠒⠛⠉⠉⠀⠀⠀⣴⠟⢃⡴⠛⠋⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠛⠛⠋⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+      </pre>
+          <p>
+            Please visit us on social media or leave a message if you have any questions or just
+            want to say hi, and we'll get back to you as soon as possible.
+          </p>
+          <p>We appreciate all the love and support!</p>
+          <pre>
+  ∧,,,∧
+( ̳• · • ̳)
+/   づ♡ We love you
+          </pre>
+          <p>Best wishes, Mångatan.</p>
+        </div>
+        <div class="image-container">
+          <img src="/assets/images/sparkles-sparkle.gif" class="sparkle" alt="sparkle" />
+          <img src="/assets/images/group-full-warm.jpg" class="contact-img" alt="Band portrait" />
+        </div>
+      </div>
+    </Window>
   </div>
 </section>
 
 <style lang="scss">
+  // TODO: refactor styles,
+  // this is way too long
+  .social-media-title {
+    font-family: 'Orbitron', sans-serif;
+    text-shadow: 1px 1px 5px rgb(255, 255, 255);
+    padding: 10px;
+    text-align: center;
+    color: white;
+    box-shadow: 1px 1px 5px rgb(49, 49, 49);
+    background-color: rgb(49, 49, 49);
+    margin: 0;
+  }
+
+  h3 {
+    font-family: 'Orbitron', sans-serif;
+    font-size: 2rem;
+  }
+
+  .left-column {
+    display: flex;
+    flex-direction: column;
+    gap: 30px;
+  }
+
+  .social-media-window {
+    margin-left: -100px;
+  }
+
+  .social-media-container {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    border: 1px solid rgb(49, 49, 49);
+    background-color: var(--win98-light-gray);
+    padding: 10px;
+
+    .stamps {
+      display: flex;
+      gap: 20px;
+    }
+  }
+
   .contact-container {
     display: flex;
+    align-items: flex-start;
     gap: 2rem;
 
     .contact-img {
-      max-width: 500px;
-      border: 2px solid var(--win98-black);
+      max-width: 100%;
+      border: 1px solid var(--win98-black);
     }
+  }
+
+  .right {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+    position: relative;
+    overflow: hidden;
+  }
+
+  .text {
+    display: flex;
+    flex-direction: column;
+    margin-bottom: 10px;
+    padding: 20px;
+    border: 1px solid rgb(0, 0, 0);
+    background-color: var(--win98-light-gray);
+  }
+
+  .cat {
+    color: white;
+    position: absolute;
+    left: 5%;
+    bottom: 0;
+  }
+
+  .sparkle {
+    position: absolute;
+    width: 100%;
+    height: auto;
+    object-fit: cover;
+    filter: grayscale(100%);
   }
 </style>
