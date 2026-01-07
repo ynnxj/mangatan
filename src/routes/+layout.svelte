2796b63c2750b53f9ab74211abcc49021e63a283
Jennifer Nilsson <jennifer.maritza.nilsson@gmail.com>
2026-01-07 17:36:20 +0100

    🐛fix(layout): add conditional rendering for landing page
    To prevent flashing of content outside of page.

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,15 +2,22 @@
   import '../app.scss';
   import Nav from '$lib/Nav/Nav.svelte';
   import SidePanel from '$lib/SideBar/SidePanel.svelte';
+  import { page } from '$app/stores';
+
+  const isLandingPage = $derived($page.url.pathname === '/');
 
   let { children } = $props();
 </script>
 
-<SidePanel side={'left'} />
+{#if !isLandingPage}
+  <SidePanel side={'left'} />
+  <main>
+    {@render children()}
+    <Nav />
+  </main>
+  <SidePanel side={'right'} />
+{/if}
 
-<main>
+{#if isLandingPage}
   {@render children()}
-  <Nav />
-</main>
-
-<SidePanel side={'right'} />
+{/if}
