ff8cd1e298f23edbeaf07334a9f4ad96bd15f218
Jennifer Nilsson <jennifer.maritza.nilsson@gmail.com>
2026-01-08 13:18:35 +0100

    🚧✨feat(merch): update merch page with styling
    Sort-function in progress.

--- src/routes/merch/+page.svelte
+++ src/routes/merch/+page.svelte
@@ -3,14 +3,33 @@
   import type { Item } from '$lib/types/merch';
 
   export let data: { merch: Item[] };
+
   const isInStock = (item: Item) => item.in_stock ?? item.stock_count > 0;
   const { merch } = data;
 </script>
 
 <section>
-  <div>
-    <h2>Merch</h2>
+  <div class="merch-page-header">
+    <img
+      class="merch-page-logo"
+      src="/assets/images/mangatan/mangatan-logo.PNG"
+      alt="Mångatan logo"
+    />
+    <img class="merch-page-title" src="/src/lib/Icons/GlitterTitle.gif" alt="Glitter merch logo" />
   </div>
+
+  <div class="sort-container">
+    <span>Sort by:</span>
+    <button class="name">Name</button>
+    <button class="price">Price</button>
+    <button class="category">Category</button>
+    <div class="category-list">
+      <p>Apparel</p>
+      <p>Accessories</p>
+      <p>Albums</p>
+    </div>
+  </div>
+
   <ul class="merch-list">
     {#if merch.length > 0}
       {#each merch as item}
@@ -41,6 +60,40 @@
 </section>
 
 <style lang="scss">
+  .merch-page-header {
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: center;
+    margin: 80px 0 40px 0;
+    border-bottom: 30px solid transparent;
+    border-image: url('/src/lib/Icons/Divider.webp') 30 repeat;
+    border-image-slice: 30;
+    border-image-width: 1;
+    border-image-outset: 0;
+
+    .merch-page-title {
+      width: 250px;
+      margin-left: 190px;
+      margin-bottom: -60px;
+      height: auto;
+      z-index: 10;
+    }
+
+    // Maybe fix this later
+    .merch-page-logo {
+      filter: invert(100%);
+      width: 500px;
+      margin-right: -360px;
+      margin-bottom: -60px;
+      z-index: 10;
+    }
+  }
+
+  .sort-container {
+    text-align: end;
+  }
+
   .merch-list {
     display: grid;
     grid-template-columns: auto auto;
