b2bb0619a61cdd097ec5c90efaecb1d5f93aa065
Jennifer Nilsson <jennifer.maritza.nilsson@gmail.com>
2026-01-08 17:40:42 +0100

    💄feat(landingpage): change enter button

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -9,7 +9,7 @@
     />
   </h1>
 
-  <a href="/about">Enter</a>
+  <a class="enter-btn" href="/about">Enter</a>
 
   <img class="grain-effect" src="/assets/images/background/grain.jpg" alt="Grain Effect" />
   <video class="background-video" autoplay loop muted>
@@ -39,25 +39,21 @@
       z-index: 10;
     }
 
-    a {
+    .enter-btn {
       position: absolute;
-      top: 60%;
-      left: 70%;
+      font-family: 'Times New Roman', Times, serif;
+
+      top: 50%;
+      left: 65%;
       padding: 10px 40px;
-      font-size: 2em;
+      font-size: 4em;
       color: var(--win98-white);
-      text-shadow: 1px 1px 2px var(--win98-black);
-      border: 1px solid var(--win98-white);
-      box-shadow: 0 0 0 1px var(--win98-dark-gray);
-      border-radius: 50px;
-      background: var(--win7-button-face);
-      text-decoration: none;
       z-index: 10;
 
       &:hover {
-        border: 1px solid var(--win98-white);
-        box-shadow: 0 0 0 1px var(--win98-light-blue);
-        background: var(--win7-button-hover);
+        color: var(--win98-accent-pink);
+        font-style: italic;
+        text-decoration: underline;
       }
     }
 
@@ -65,7 +61,7 @@
       width: 100vw;
       height: 100vh;
       object-fit: cover;
-      filter: sepia(100%) hue-rotate(180deg) saturate(500%) brightness(50%) contrast(70%);
+      filter: sepia(100%) hue-rotate(180deg) saturate(350%) brightness(60%) contrast(80%);
     }
 
     .grain-effect {
@@ -77,7 +73,7 @@
       object-fit: cover;
       opacity: 0.2;
       z-index: 1;
-      filter: brightness(5) contrast(0.5) opacity(0.6);
+      filter: pacity(1);
     }
   }
 </style>
