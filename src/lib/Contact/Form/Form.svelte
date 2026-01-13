21db780dc7642383781b09cb4273409604403b5c
Jennifer Nilsson <jennifer.maritza.nilsson@gmail.com>
2026-01-13 19:03:18 +0100

    ✨feat(contact): add feedback modal on submit message

--- src/lib/Contact/Form/Form.svelte
+++ src/lib/Contact/Form/Form.svelte
@@ -1,6 +1,28 @@
+<script lang="ts">
+  import Window from '$lib/WindowBorder/Window.svelte';
+  import { scroll } from '$lib/utils/scroll';
+
+  let showFeedbackModal = $state(false);
+
+  const handleSubmit = (e: Event) => {
+    e.preventDefault();
+
+    const formElement = e.target as HTMLFormElement;
+    formElement.reset();
+
+    showFeedbackModal = true;
+  };
+
+  // Scroll toggle
+  $effect(() => {
+    scroll.toggle(showFeedbackModal);
+    return () => scroll.unlock();
+  });
+</script>
+
 <!--TODO: Add accessibility attributes to all form elements 
 and ensure proper labeling for screen readers -->
-<form method="post" action="/contact">
+<form method="post" action="/contact" onsubmit={handleSubmit}>
   <label for="name">Name:</label>
   <input type="text" aria-label="Name" placeholder="Full name" required />
 
@@ -16,7 +38,64 @@ and ensure proper labeling for screen readers -->
   </div>
 </form>
 
+{#if showFeedbackModal}
+  <button
+    class="confirm-overlay"
+    onclick={() => (showFeedbackModal = false)}
+    aria-label="Confirm submit message overlay"
+  ></button>
+
+  <div class="confirm-submit-message">
+    <!-- TODO: Maybe make this a reusable component -->
+    <Window windowTitle="Message Sent">
+      <div class="submit-modal">
+        <div class="submit-title">
+          <h4>Thank you for your message!</h4>
+          <pre>⊹₊ ˚‧︵‿₊୨୧₊‿︵‧˚ ₊⊹</pre>
+        </div>
+
+        <div class="submit-text">
+          <p>Well get back to you as soon as possible!</p>
+          <p>Love, Mångatan</p>
+        </div>
+      </div>
+    </Window>
+  </div>
+{/if}
+
 <style lang="scss">
+  .confirm-overlay {
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background-color: rgba(0, 0, 0, 0.5);
+    backdrop-filter: blur(2px);
+    z-index: 20;
+  }
+
+  .confirm-submit-message {
+    position: fixed;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    z-index: 50;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+
+    .submit-title {
+      font-size: 3rem;
+      padding: 0 0px;
+
+      pre {
+        font-size: 2rem;
+        text-align: center;
+      }
+    }
+  }
+
   form {
     margin-top: 10px;
     display: flex;
@@ -52,23 +131,17 @@ and ensure proper labeling for screen readers -->
         font-size: 1.2rem;
         font-family: 'Orbitron', sans-serif;
         align-self: center;
-        text-shadow: 1px 1px 2px black;
-        border: 1px solid #f3f3f3;
-        box-shadow: 0 0 0 1px #707070;
+        text-shadow: 1px 1px 2px var(--win98-black);
+        border: 1px solid var(--win98-white);
+        box-shadow: 0 0 0 1px var(--win98-dark-gray);
         border-radius: 15px;
-        background: linear-gradient(to bottom, #f2f2f2 0%, #d4d6e6 50%, #8996bb 51%, #cfcfcf 100%);
-        color: white;
+        background: var(--win7-button-face);
+        color: var(--win98-white);
 
         &:hover {
-          border: 1px solid #ecf7fd;
-          box-shadow: 0 0 0 1px #3c7fb1;
-          background: linear-gradient(
-            to bottom,
-            #eaf6fd 0%,
-            #d9f0fc 50%,
-            #bee6fd 51%,
-            #a7d9f5 100%
-          );
+          border: 1px solid var(--win98-white);
+          box-shadow: 0 0 0 1px var(--win98-light-blue);
+          background: var(--win7-button-hover);
         }
       }
     }
