90d8b569884e617bd8132521fb6a41f7c0ce59df
Jennifer Nilsson <jennifer.maritza.nilsson@gmail.com>
2026-01-16 22:41:47 +0100

    ♻️refactor(routes): clean up rout
    Add highligt to about page and remove useless section tag

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -3,19 +3,13 @@
   import type { Gig } from '$lib/types/gigs';
   import { page } from '$app/stores';
   import About from '$lib/About/About.svelte';
+  import Highlight from '$lib/About/Highlight/Highlight.svelte';
 
   let gigs: Gig[] = $page.data?.gigs || [];
 </script>
 
 <section class="about-container">
   <About />
+  <Highlight />
   <GigsList {gigs} />
 </section>
-
-<style lang="scss">
-  .about-container {
-    display: flex;
-    flex-direction: column;
-    gap: 20px;
-  }
-</style>
--- src/routes/contact/+page.svelte
+++ src/routes/contact/+page.svelte
@@ -2,6 +2,4 @@
   import Contact from '$lib/Contact/Contact.svelte';
 </script>
 
-<section>
-  <Contact />
-</section>
+<Contact />
