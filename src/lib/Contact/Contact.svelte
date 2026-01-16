c16a89c783a434ad2e9bb0a381d234ef91b49213
Jennifer Nilsson <jennifer.maritza.nilsson@gmail.com>
2026-01-16 22:46:45 +0100

    ♻️🚚refactor(contact): move to seperate files
    Improve file and folder structure and remove unsued styling.

--- src/lib/Contact/Contact.svelte
+++ src/lib/Contact/Contact.svelte
@@ -1,19 +1,9 @@
 <script lang="ts">
-  import ContactForm from './ContactForm.svelte';
+  import ContactForm from './Form/ContactForm.svelte';
   import Message from './Message.svelte';
-  import SocialMedia from './SocialMedia.svelte';
 </script>
 
 <div class="contact-container">
   <Message />
   <ContactForm />
-  <SocialMedia />
 </div>
-
-<style lang="scss">
-  .contact-container {
-    display: flex;
-    flex-direction: column;
-    gap: 20px;
-  }
-</style>
---
+++ src/lib/Contact/Form/ContactForm.svelte
@@ -0,0 +1,70 @@
+<script lang="ts">
+  import Window from '$lib/WindowBorder/Window.svelte';
+  import { scroll } from '$lib/utils/scroll';
+  import './contact-form.scss';
+
+  let showFeedbackModal = $state(false);
+
+  const handleSubmit = (e: Event) => {
+    e.preventDefault();
+
+    const formElement = e.target as HTMLFormElement;
+    formElement.reset();
+
+    showFeedbackModal = true;
+  };
+
+  // Scroll toggle
+  $effect(() => {
+    scroll.toggle(showFeedbackModal);
+    return () => scroll.unlock();
+  });
+</script>
+
+<!--TODO: Add accessibility attributes to all form elements 
+and ensure proper labeling for screen readers -->
+
+<Window windowTitle={'Contact Us'}>
+  <h3 class="form-title">Send a Message</h3>
+
+  <form method="post" action="/contact" onsubmit={handleSubmit}>
+    <label for="name">Name:</label>
+    <input type="text" aria-label="Name" placeholder="Full name" required />
+
+    <label for="email">Email:</label>
+    <input type="email" aria-label="Email" name="email" placeholder="Email adress" required />
+
+    <label for="message">Message:</label>
+    <textarea aria-label="Message" placeholder="Write something..." required></textarea>
+
+    <div class="contact-buttons">
+      <button type="submit">Send</button>
+      <button type="reset">Reset</button>
+    </div>
+  </form>
+
+  {#if showFeedbackModal}
+    <button
+      class="confirm-overlay"
+      onclick={() => (showFeedbackModal = false)}
+      aria-label="Confirm submit message overlay"
+    ></button>
+
+    <div class="confirm-submit-message">
+      <!-- TODO: Maybe make this a reusable component -->
+      <Window windowTitle="Message Sent">
+        <div class="submit-modal">
+          <div class="submit-title">
+            <h4>Thank you for your message!</h4>
+            <pre>⊹₊ ˚‧︵‿₊୨୧₊‿︵‧˚ ₊⊹</pre>
+          </div>
+
+          <div class="submit-text">
+            <p>Well get back to you as soon as possible!</p>
+            <p>Love, Mångatan</p>
+          </div>
+        </div>
+      </Window>
+    </div>
+  {/if}
+</Window>
