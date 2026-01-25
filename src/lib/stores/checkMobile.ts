825be5848f92d1b3d51cd90172ccdd0e2b478115
Jennifer Nilsson <jennifer.maritza.nilsson@gmail.com>
2026-01-25 13:21:48 +0100

    ✨feat(utils): create isMobile store

---
+++ src/lib/stores/checkMobile.ts
@@ -0,0 +1,9 @@
+import { writable } from 'svelte/store';
+
+export const isMobile = writable(false);
+
+if (typeof window !== 'undefined') {
+  const handleResize = () => isMobile.set(window.innerWidth <= 768);
+  window.addEventListener('resize', handleResize);
+  handleResize();
+}
