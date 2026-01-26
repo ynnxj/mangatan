9f85a3db96f30c0c5f0bbcf501994b0fa6467e51
Jennifer Nilsson <jennifer.maritza.nilsson@gmail.com>
2026-01-26 10:24:49 +0100

    ♻️refactor(about): improve structure inside +page.svelte

--- src/lib/About/About.svelte
+++ src/lib/About/About.svelte
@@ -1,10 +1,23 @@
 <script lang="ts">
-  import Window from '$lib/WindowBorder/Window.svelte';
-  import AboutBand from './Band/AboutBand.svelte';
+  import GigsList from '$lib/Gigs/GigsList.svelte';
+  import Highlight from '$lib/About/Highlight/Highlight.svelte';
+  import News from '$lib/About/News/News.svelte';
+  import { isMobile } from '$lib/stores/checkMobile';
   import Members from './Member/Members.svelte';
+  import AboutBand from './Band/AboutBand.svelte';
+  import Window from '$lib/WindowBorder/Window.svelte';
+
+  const { newsData, gigsData } = $props<{ newsData: any[]; gigsData: any[] }>();
 </script>
 
-<Window windowTitle="About Us" width="600px">
-  <AboutBand />
-  <Members />
-</Window>
+<section class="about-container">
+  <Window windowTitle="About Us" width="600px">
+    <AboutBand />
+    <Members />
+  </Window>
+  <Highlight />
+  {#if $isMobile}
+    <News news={newsData} />
+  {/if}
+  <GigsList gigs={gigsData} />
+</section>
--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -1,30 +1,6 @@
 <script lang="ts">
-  import GigsList from '$lib/Gigs/GigsList.svelte';
   import About from '$lib/About/About.svelte';
-  import Highlight from '$lib/About/Highlight/Highlight.svelte';
-  import News from '$lib/About/News/News.svelte';
-  import { isMobile } from '$lib/stores/checkMobile';
-
-  const props = $props();
-
-  const news = $derived(() => props.data?.news ?? []),
-    gigs = $derived(() => props.data?.gigs ?? []);
+  export let data: { news: any[]; gigs: any[] };
 </script>
 
-<section class="about-container">
-  <About />
-  <Highlight />
-  {#if $isMobile}
-    <News news={news()} />
-  {/if}
-  <GigsList gigs={gigs()} />
-</section>
-
-<style lang="scss">
-  .about-container {
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    justify-content: center;
-  }
-</style>
+<About newsData={data.news} gigsData={data.gigs} />
