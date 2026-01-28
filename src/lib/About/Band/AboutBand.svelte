8e0f66badf6a88a23399c533f4665e300730c3fc
Jennifer Nilsson <jennifer.maritza.nilsson@gmail.com>
2026-01-28 11:26:45 +0100

    💄feat(about): update styling

--- src/lib/About/Band/AboutBand.svelte
+++ src/lib/About/Band/AboutBand.svelte
@@ -1,14 +1,13 @@
 <script lang="ts">
-  import SpinningCd from '$lib/Icons//SmallIcons/SpinningCd.gif';
   import './about-band.scss';
 </script>
 
 <!-- About the Band Section-->
-<h2 class="about-greeting-title">―⋆Welcome⋆˙</h2>
+<h2 class="about-greeting-title">⋆Welcome⋆˙―</h2>
 <div class="about">
   <img
     class="image"
-    src="/assets/images/mangatan/group-portrait-warm.jpg"
+    src="/assets/images/mangatan/about-img.jpg"
     alt="Group portrait of the band Mångatan"
     width="828"
     height="853"
@@ -16,14 +15,13 @@
 
   <!--About Text -->
   <div class="about-text">
-    <h3 class="about-title">About Mångatan</h3>
+    <h3 class="about-title">About Us</h3>
     <p>
       Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea aperiam dolorem assumenda ad
       perspiciatis dignissimos numquam vitae tenetur aliquam, mollitia iste laudantium, debitis
       molestias officia doloribus, neque dolor atque ipsum. Lorem ipsum dolor sit amet consectetur
       adipisicing elit.
     </p>
-    <img class="icon" src={SpinningCd} alt="Spinning CD" width="20" height="20" />
     <p>
       Ea aperiam dolorem assumenda ad perspiciatis dignissimos numquam vitae tenetur aliquam,
       mollitia iste laudantium, debitis molestias officia doloribus, neque dolor atque ipsum.
